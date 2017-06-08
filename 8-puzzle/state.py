fe919a149ad9e22362e20fbec96334975634beda
cerious <your.ceriousn@yahoo.com>
2017-06-07 21:42:50 -0400

    class method wont accept list as argument.

--- 8-puzzle/state.py
+++ 8-puzzle/state.py
@@ -1,62 +1,74 @@
 class State:
-    def __init__(self):
-        self.state = []
+####how to access instace variables within a class
+    
+    def __init__(self, arr):
+        self.state = arr
+        
+        
+    def swap(arr, x, y):
+        arr[x], arr[y] = arr[y], arr[x]
 
-    end
-   
-    def swap(x,y):
-        self.state[x], self.state[y] = self.state[y], self.state[x]
+    
+    def neighbors(arr):
+        neighbor = []
+        if arr.index(0) == 0:
+            swap(arr,0, 1)
+            neighbor.append(arr)
+            swap(arr,0, 3)
+            neighbor.append(arr)
+        elif arr.index(0) == 1:
+            swap(arr, 1, 0)
+            neighbor.append(arr)
+            swap(arr, 1, 4)
+            neighbor.append(arr)
+            swap(arr, 1, 2)
+            neighbor.append(arr)
+        elif arr.index(0) == 2:
+            swap(arr, 2, 1)
+            neighbor.append(arr)
+            swap(arr, 2, 5)
+            neighbor.append(arr)
+        elif arr.index(0) == 3:
+            swap(arr, 3, 0)
+            neighbor.append(arr)
+            swap(arr, 3, 4)
+            neighbor.append(arr)
+            swap(arr, 3, 6)
+        elif arr.index(0) == 5:
+            swap(arr, 5, 2)
+            neighbor.append(arr)
+            swap(arr, 5, 8)
+            neighbor.append(arr)
+            swap(arr, 5, 4)
+            neighbor.append(arr)
+        elif arr.index(0) == 6:
+            swap(arr, 6, 3)
+            neighbor.append(arr)
+            swap(arr, 6, 7)
+            neighbor.append(arr)
+        elif arr.index(0) == 7:
+            swap(arr, 7, 4)
+            neighbor.append(arr)
+            swap(arr, 7, 6)
+            neighbor.append(arr)
+            swap(arr, 7, 8)
+            neighbor.append(arr)
+        elif arr.index(0) == 8:
+            swap(arr, 8, 5)
+            neighbor.append(arr)
+            swap(arr, 8, 7)
+            neighbor.append(arr)
+    
+        neighbor
 
-    self.neighbors =  []
-
+        
+    
 ### swapping rules
-    if self.state.index(0) == 0:
-        swap(0, 1)
-        neighbors.append(self.state)
-        swap(0, 3)
-        neighbors.append(self.state)
-    elif self.state.index(0) == 1:
-        swap(1, 0)
-        neighbors.append(self.state)
-        swap(1, 4)
-        neighbors.append(self.state)
-        swap(1, 2)
-        neighbors.append(self.state)
-    elif self.state.index(0) == 2:
-        swap(2, 1)
-        neighbors.append(self.state)
-        swap(2, 5)
-        neighbors.append(self.state)
-    elif self.state.index(0) == 3:
-        swap(3, 0)
-        neighbors.append(self.state)
-        swap(3, 4)
-        neighbors.append(self.state)
-        swap(3, 6)
-    elif self.state.index(0) == 5:
-        swap(5, 2)
-        neighbors.append(self.state)
-        swap(5, 8)
-        neighbors.append(self.state)
-        swap(5, 4)
-        neighbors.append(self.state)
-    elif self.state.index(0) == 6:
-        swap(6, 3)
-        neighbors.append(self.state)
-        swap(6, 7)
-        neighbors.append(self.state)
-    elif self.state.index(0) == 7:
-        swap(7, 4)
-        neighbors.append(self.state)
-        swap(7, 6)
-        neighbors.append(self.state)
-        swap(7, 8)
-        neighbors.append(self.state)
-    elif self.state.index(0) == 8:
-        swap(8, 5)
-        neighbors.append(self.state)
-        swap(8, 7)
-        neighbors.append(self.state)
+    
 
 
-class Solver:
+
+initial = State([1,2,5,3,4,0,6,7,8])
+print(initial.state)
+print(initial.neighbors(initial.state)
+###class Solver:
